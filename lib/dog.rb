ca1bfbe55c45e118394cf3eeabc8beea3236ced0
Tracy Ngo <tngo1206@gmail.com>
2019-04-27 23:47:34 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,34 @@
+class Dog 
+  attr_accessor :name, :breed, :id
+
+  def initialize(args)
+    @name = args[:name]
+    @breed = args[:breed]
+    @id = args[:id]
+  end
+  
+  def self.create_table
+    sql_table = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    )
+    SQL
+    
+    DB[:conn].execute(sql_table)
+  end
+
+  def self.drop_table
+    sql_table = <<-SQL
+    DROP TABLE dogs
+    )
+    SQL
+    
+    DB[:conn].execute(sql_table)
+  end
+  
+  def self.new_from_db(row)
+    
+  end
+end
