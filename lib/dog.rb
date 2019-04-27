5c1ac5ed3d72e685c1a4b890442143c9bf82dc01
Tracy Ngo <tngo1206@gmail.com>
2019-04-27 23:51:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,7 @@ class Dog
   end
   
   def self.new_from_db(row)
-    dog = Dog.new(id => row[0], name => row[1], row[2])
+    dog = Dog.new(id => row[0], name => row[1], breed => row[2])
     dog
   end
   
