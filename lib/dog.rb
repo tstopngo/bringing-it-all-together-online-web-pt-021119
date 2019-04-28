50e543f6d21483955a019b4179d335681dbfcc98
Tracy Ngo <tngo1206@gmail.com>
2019-04-28 00:20:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,6 +69,15 @@ class Dog
   def 
   
   def self.find_by_name(name)
-  
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
