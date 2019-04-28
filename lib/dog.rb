b201df75070fd83eb6d95e0a5a95dc2a430fc258
Tracy Ngo <tngo1206@gmail.com>
2019-04-28 00:38:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -95,6 +95,6 @@ class Dog
     sql_query = <<-SQL
       UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
-    DB[:conn].execute(sql_query,self.name, self.breed, self.id).map
+    DB[:conn].execute(sql_query,self.name, self.breed, self.id)
   end
 end
