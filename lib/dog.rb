b59d122375d61386a772c55741597397b258495a
Tracy Ngo <tngo1206@gmail.com>
2019-04-28 00:22:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,7 +79,7 @@ class Dog
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql,name).map do |row|
+    DB[:conn].execute(sql_query,name).map do |row|
       self.new_from_db(row)
     end.first
   end
