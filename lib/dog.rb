68fcfb5d4373c2217b927ebf745c61fe97ee8187
Tracy Ngo <tngo1206@gmail.com>
2019-04-28 00:12:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,8 @@ class Dog
     SQL
     
     search = DB[:conn].execute(sql_query, id)[0]
-    dog =Dog.new(search[0], search[1], search[2])
+    dog = Dog.new(search[0], search[1], search[2])
+    dog
   end
   
   def self.find_by_name(name)
